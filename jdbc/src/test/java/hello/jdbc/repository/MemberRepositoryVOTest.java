7e4570fb6a287090413b6268a5ea59a75248df3a
한영수 <besthys7@gmail.com>
2022-12-28 15:34:28 +0900

    JDBC 개발 - 수정, 삭제

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,12 +18,23 @@ class MemberRepositoryVOTest {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("faker", 10000);
+        Member member = new Member("deft", 10000);
         repository.save(member);
 
         //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 39999);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(39999);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
     }
 }
